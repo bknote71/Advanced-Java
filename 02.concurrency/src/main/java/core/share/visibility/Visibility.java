4e0d9d873892a17c054212cd76d8f9cfe4d2fb7c
bknote71 <bknote71@gmail.com>
2022-11-11 17:48:04 +0900

    ambiguous visibility

--- 02.concurrency/src/main/java/core/share/visibility/Visibility.java
+++ 02.concurrency/src/main/java/core/share/visibility/Visibility.java
@@ -8,9 +8,10 @@ package core.share.visibility;
  * --> 최신의 데이터를 가져오려면 getter에 synchronized를 설정해야 한다.
  * 
  * setter에만 synchronized 설정 시:
- * - synchronized 블럭을 진입할 때에는 쓰기 연산이 없어 업데이트할 연산이 없다. (쓰기 연산은 setter 하나이므로 그 전에는 없다. 물론 그 전에 쓰기 연산이 있으면 업데이트 된다.)
  * - synchronized 블럭을 벗어날 때 메인 메모리로 데이터를 flush 한다.
  * - 하지만 getter에는 synchronized가 없기 때문에 메인 메모리로부터 데이터를 가져올 수 없다.
+ * --> 이렇게 예측했었는데 틀렸다.
+ * --> while문에 내용이 없으면 메인 메모리로부터 데이터를 가져오지 않는데, 내용이 있으면 메인 메모리로부터 데이터를 가져온다.
  *
  * getter에만 synchronized 설정 시:
  * - synchronized 블럭을 진입할 때 블럭 전까지의 모든 쓰기 연산이 업데이트 되고 메인 메모리로부터 데이터를 가져오도록 한다
--- 02.concurrency/src/test/java/core/share/visibility/VisibilityTest.java
+++ 02.concurrency/src/test/java/core/share/visibility/VisibilityTest.java
@@ -35,9 +35,9 @@ public class VisibilityTest {
 
         final Thread thread = new Thread(() -> {
             while (visibility.getHalfStatus() == 0) {
-                // ...
+                System.out.printf("...");
             }
-            System.out.println("out2");
+            System.out.println("\nout2");
         });
 
         thread.start();
@@ -63,6 +63,27 @@ public class VisibilityTest {
         Thread.sleep(100);
 
         visibility.setVolatileStatus(1);
+        thread.join();
+    }
+
+    @Test
+    public void volatileFlushTest() throws InterruptedException {
+        final Visibility visibility = new Visibility();
+
+        final Thread thread = new Thread(() -> {
+            while (visibility.getHalfStatus() == 0) {
+                System.out.printf("...");
+            }
+            System.out.println("\nout4");
+        });
+
+        thread.start();
+        Thread.sleep(100);
+
+//        visibility.setHalfStatus(1);
+        visibility.getVolatileStatus();
+//        visibility.setVolatileStatus(1);
+        visibility.setHalfStatus(1);
         thread.join();
     }
 
