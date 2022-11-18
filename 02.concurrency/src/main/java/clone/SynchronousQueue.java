d9216cc6aba5030941772d89f4c301a61d403ebc
bknote71 <bknote71@gmail.com>
2022-11-18 19:08:25 +0900

    await fulfill

--- 02.concurrency/src/main/java/clone/SynchronousQueue.java
+++ 02.concurrency/src/main/java/clone/SynchronousQueue.java
@@ -10,6 +10,12 @@ public class SynchronousQueue {
         abstract E transfer(E e, boolean timed, long nanos);
     }
 
+    static final int MAX_TIMED_SPINS =
+            (Runtime.getRuntime().availableProcessors() < 2) ? 0 : 32;
+
+    static final int MAX_UNTIMED_SPINS = MAX_TIMED_SPINS * 16;
+    static final long SPIN_FOR_TIMEOUT_THRESHOLD = 1000L;
+
     static class TransferStack<E> extends Transferer<E> {
 
         // mode
@@ -91,11 +97,16 @@ public class SynchronousQueue {
                 Node h = head;
                 if (h == null || h.mode == mode) {
                     if (casHead(h, n = node(n, e, h, mode))) {
-                        // current thread 설정
+                        // 대기: waiter thread, spin/block
+                        // m: 매치한 노드
+                        Node m = awaitFulfill(n, timed, nanos);
+
+                        return (E) (mode == DATA ? n.item : m.item);
                     }
                 } else if ((h.mode & FULFILLING) == 0) { // 2. mode도 다르고 fulfill도 아니다.
                     // fulfill 모드 추가하여 push: casHead(h, newNode);
                     if (casHead(h, n = node(n, e, h, mode | FULFILLING))) { // matching...
+
                         Node m = n.next;
                         Node mn = m.next;
 
@@ -110,6 +121,55 @@ public class SynchronousQueue {
                     // waiting ..
                 }
             }
+        }
+
+        private Node awaitFulfill(Node n, boolean timed, long nanos) {
+            // spin vs block
+            // spin: 말 그대로 loop 반복
+            // block: LockSupport.park pr LockSupport.parkNanos() 나노초만큼 대기?
+            // fulfill 을 대기하는데 모드가 다른 상대측에서 waiter 스레드를 꺼내고 LockSupport.unpark(thread) 해주면 블락 통과
+            
+            // 대기하다가 n.match 가 null 이 아니면 된다.
+
+            // timed true: 특정 시간만큼 주차, false: 무제한 주차
+            final long deadline = timed ? System.nanoTime() + nanos : 0L;
+            Thread t = Thread.currentThread();
+
+            // spin: 짧은 시간이 예상되면 spin
+            int spins = shouldSpin(n)
+                    ? (timed ? MAX_TIMED_SPINS : MAX_UNTIMED_SPINS)
+                    : 0;
+
+            for (;;) {
+                Node m = n.match;
+
+                if (m != null)
+                    return m;
+
+                if(timed) { // nanos 갱신
+                    nanos = deadline - System.nanoTime();
+                    if (nanos <= 0) {
+                        // ? 취소 ?
+                    }
+                }
+
+                if (spins > 0) {
+                    Thread.onSpinWait(); // 이건 뭐죠?
+                    spins = shouldSpin(n) ? (spins - 1) : 0;
+                }
+                else if (n.waiter == null)
+                    n.waiter = t;
+                else if (!timed) // 무제한 주차
+                    LockSupport.park();
+                else if (nanos > SPIN_FOR_TIMEOUT_THRESHOLD)
+                    LockSupport.parkNanos(this, nanos);
+            }
+        }
+
+        // node 가 head 이거나 fulfill 중이면?
+        private boolean shouldSpin(Node n) {
+            Node h = head;
+            return (h == n || (h.mode & FULFILLING) != 0);
         }
 
         private static final VarHandle HEAD;
